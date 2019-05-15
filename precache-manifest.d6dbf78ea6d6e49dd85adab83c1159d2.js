41f6c7b5e984bf2361668e993ca9a0d9ce1b82be
BrandonJoe <brandon.joe42@yahoo.com>
2019-05-14 18:06:18 -0700

    Updates

---
+++ precache-manifest.d6dbf78ea6d6e49dd85adab83c1159d2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8cc5dc7d148761e3cdf3bb50f017169",
+    "url": "/static/media/landing.f8cc5dc7.jpg"
+  },
+  {
+    "revision": "6578fbb5372028fe1802",
+    "url": "/static/css/main.a0f42c94.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4928afd41b4fbe1d4d089b99831303b5",
+    "url": "/static/media/cherry.4928afd4.png"
+  },
+  {
+    "revision": "98479f3ead544a8cdf31",
+    "url": "/static/js/2.98479f3e.chunk.js"
+  },
+  {
+    "revision": "fee8a0d07cd8ec1cbe19340f2222c77a",
+    "url": "/static/media/stats.fee8a0d0.jpg"
+  },
+  {
+    "revision": "6578fbb5372028fe1802",
+    "url": "/static/js/main.6578fbb5.chunk.js"
+  },
+  {
+    "revision": "394a1cf2bdc1f13efdcbd15f42791d13",
+    "url": "/static/media/features1.394a1cf2.jpg"
+  },
+  {
+    "revision": "0c256b34217a7f1a1f2d628d159f8b23",
+    "url": "/static/media/about1.0c256b34.jpg"
+  },
+  {
+    "revision": "e6d3d062dc9b56ac4378459fef02a3e3",
+    "url": "/static/media/about2.e6d3d062.jpg"
+  },
+  {
+    "revision": "6df3b3c2a93976d08823100d63762585",
+    "url": "/static/media/about3.6df3b3c2.jpg"
+  },
+  {
+    "revision": "98479f3ead544a8cdf31",
+    "url": "/static/css/2.455dcb04.chunk.css"
+  },
+  {
+    "revision": "f6a322198d1e0189f96071a9cc3d77f2",
+    "url": "/index.html"
+  }
+];
